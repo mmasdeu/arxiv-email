96cd3ef860cc3df2b40a7ea8b2e4a5d0f206f0fd
Marc Masdeu <marc.masdeu@gmail.com>
2024-12-03 14:11:53 +0100

    Changed mathjax import

--- emailgen.py
+++ emailgen.py
@@ -138,7 +138,7 @@ def send_email(recipient_email,subscription_preferences,sender_email,sender_pass
     renderMathInElement(document.body,{delimiters: [{left: "$$", right: "$$", display: true},{left: "$", right: "$", display: false}]});
 </script>
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+<script id="MathJax-script" async src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
 <script>
  window.MathJax = {
   tex: {
